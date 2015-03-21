1f2321de3221a7ac236f7fe78a82d4fd3c375c25
Michael Hsu <pig0826usanj@gmail.com>
2015-03-20 21:37:51 -0700

    added passport oauth2 code

--- appserver/index.js
+++ appserver/index.js
@@ -1,10 +1,36 @@
 var express = require('express');
-var config = require('./config');
 var app = express();
+var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
+var config = require('./config');
+
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://127.0.0.1:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return done(err, user);
+    });
+  }
+));
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 });
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: 'https://www.googleapis.com/auth/plus.login' }));
+
+app.get('/auth/google/callback', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
 
 var server = app.listen(3000, function () {
 
@@ -14,3 +40,5 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 });
+
+
