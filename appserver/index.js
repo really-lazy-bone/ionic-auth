2425028b9fe05e81a1e7b1829c971aa2c7c0ce8a
Michael Hsu <pig0826usanj@gmail.com>
2015-03-20 21:59:56 -0700

    return profile to client

--- appserver/index.js
+++ appserver/index.js
@@ -5,14 +5,33 @@ var config = require('./config');
 
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Google profile is
+//   serialized and deserialized.
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://127.0.0.1:3000/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
-    User.findOrCreate({ googleId: profile.id }, function (err, user) {
-      return done(err, user);
+      process.nextTick(function () {
+      
+     
+      return done(null, profile);
     });
   }
 ));
@@ -40,5 +59,3 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 
 });
-
-
